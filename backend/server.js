057f2fd2aaea59e90b2a66b00533f77984f7f849
Kappala Sai Mohith <113962391+kappalasaimohith@users.noreply.github.com>
2025-05-03 14:09:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,9 @@ import { routes } from './routes.js';
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors(
+  origin: 'https://online-compiler-frontend-phi.vercel.app',
+));
 app.use(bodyParser.json());
 
 app.use('/api', routes);
